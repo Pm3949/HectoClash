dad5d1d200112714a546bee03529adbacc4eb0c6
Pm3949 <manavpatel0767@gmail.com>
2025-05-25 22:59:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Frontend URL
+  origin: 'https://hectoclash-cuwf.onrender.com', // Frontend URL
   credentials: true,
 };
 app.use(cors(corsOptions));

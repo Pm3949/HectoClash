3a5794c3ef0df09d418d0692e268067dfd24a635
Pm3949 <manavpatel0767@gmail.com>
2025-05-25 22:16:02 +0530

    Update socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080", {
+const socket = io("https://hectoclash-backend.onrender.com", {
   withCredentials: true,
   autoConnect:true,
 });
